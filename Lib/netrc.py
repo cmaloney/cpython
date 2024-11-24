6987cde186ba3912767c9dde1eed247b3d038b55
Cody Maloney <cmaloney@theoreticalchaos.com>
2024-11-23 23:59:55 -0800

    Newparser from scratch

--- Lib/netrc.py
+++ Lib/netrc.py
@@ -21,48 +21,202 @@ class NetrcParseError(Exception):
         return "%s (%s, line %s)" % (self.msg, self.filename, self.lineno)
 
 
-class _netrclex:
-    def __init__(self, fp):
-        self.lineno = 1
-        self.instream = fp
-        self.whitespace = "\n\t\r "
-        self.pushback = []
+def _process_escapes(token):
+    """Single-pass escape removal to avoid copies.
 
-    def _read_char(self):
-        ch = self.instream.read(1)
-        if ch == "\n":
-            self.lineno += 1
-        return ch
-
-    def get_token(self):
-        if self.pushback:
-            return self.pushback.pop(0)
-        token = ""
-        fiter = iter(self._read_char, "")
-        for ch in fiter:
-            if ch in self.whitespace:
-                continue
-            if ch == '"':
-                for ch in fiter:
-                    if ch == '"':
-                        return token
-                    elif ch == "\\":
-                        ch = self._read_char()
-                    token += ch
-            else:
-                if ch == "\\":
-                    ch = self._read_char()
-                token += ch
-                for ch in fiter:
-                    if ch in self.whitespace:
-                        return token
-                    elif ch == "\\":
-                        ch = self._read_char()
-                    token += ch
+    Most netrc keywords don't contain escapes. (ex. machine, user, login, ...)
+    """
+    if "\\" not in token:
         return token
 
-    def push_token(self, token):
-        self.pushback.append(token)
+    unescaped = ""
+    for char in token:
+        if char == "\\":
+            continue
+        unescaped += char
+    return unescaped
+
+
+class _netrcparse:
+    def __init__(self, file, fp):
+        # NOTE: Relies on universal newlines to count lineno post-parse as well
+        # as normalize line endings across platforms.
+        self.instream = fp
+        assert fp.newlines is None, "Doesn't allow arbitrary readline."
+        self.file = file
+        self.whitespace = "\n\t\r "
+        self.all_text = self.instream.read()
+        self.remaining = self.all_text
+        self.next_token_end = 0
+        self.next_token = None
+        self.bytes_consumed = 0
+
+    def get_lineno(self):
+        return self.all_text[:self.bytes_consumed].count("\n")
+
+    def _make_error(self, msg):
+        return NetrcParseError(msg, self.file, self.get_lineno())
+
+    def _consume(self):
+        self.bytes_consumed += self.next_token_end
+        self.remaining = self.remaining[self.next_token_end:]
+        # assert self.bytes_consumed + len(self.remaining) == len(self.all_text), \
+        #    "Shouldn't be loosing data ever."
+        self.next_token_end = 0
+        self.next_token = None
+
+
+    def _lex_quotes(self):
+        """Read until quote that is not preceeded by an escape."""
+        self.next_token_end += 1
+        while True:
+            next_newline = self.remaining[self.next_token_end:].find('"')
+            match next_newline:
+                case -1:
+                    # TODO/FIXME: This wasn't handled in old code...
+                    raise self._make_error("TODO: Unexpected quote end %r" % self.remaining)
+                case _ as offset:
+                    # If the newline was escaped, ignore it
+                    if self.remaining[self.next_token_end+offset-1] == '\\':
+                        continue
+                    # Found end of string, return the string contents.
+                    unquoted = self.remaining[1:self.next_token_end-1]
+                    return _process_escapes(unquoted)
+
+    def _find_next_token(self):
+        """Move to the start of the next token, but don't consume."""
+
+        while True:
+            assert self.next_token_end == 0, \
+                "Shouldn't be in a token / last token should be consumed."
+
+            # End of file/buffer
+            if not self.remaining:
+                return ""
+
+            match self.remaining[0]:
+                # Comments
+                case '#':
+                    match self.remaining.find("\n"):
+                        case -1:
+                            # Comment into EOF, no further tokens.
+                            self.next_token_end += len(self.remaining)
+                        case _ as next_newline:
+                            self.next_token_end += next_newline
+                    self._consume()
+
+                # Whitespace
+                case c if c in self.whitespace:
+                    # Find first non-whitespace.
+                    # FIXME/TODO: Can we do a faster find method?
+                    while self.next_token_end < len(self.remaining) \
+                        and self.remaining[self.next_token_end] in c:
+                        self.next_token_end += 1
+                    self._consume()
+
+                # Tokens, either quoted or literals
+                case '"':
+                    return self._lex_quotes()
+                case _:
+                    # Read until whitespace which doesn't have an escape.
+                    self.next_token_end += 1
+                    while self.remaining[self.next_token_end] not in self.whitespace:
+                        # Skip all escaped characters
+                        if self.remaining[self.next_token_end] == '\\':
+                            # FIXME/TODO: This will error at EOF currently.
+                            self.next_token_end += 1
+
+                        self.next_token_end += 1
+
+                    return _process_escapes(self.remaining[:self.next_token_end])
+
+
+    def _peek_token(self):
+        """Move to the start of the next token, but don't consume."""
+        # Already have a peeked token
+        if self.next_token is not None:
+            return self.next_token
+
+        self.next_token = self._find_next_token()
+        assert self.next_token is not None, \
+            "Should have gotten a token or exception."
+        assert not self.remaining or self.next_token_end != 0, \
+            "Should either have no data remaining, or "
+
+        # DEBUG: print(f"TOKEN {self.next_token}, {self.remaining[:20] = }")
+        return self.next_token
+
+
+    def _consume_token(self):
+        """Consume and return the next token."""
+        token = self._peek_token()
+        self._consume()
+        return token
+
+    def _parse_macro(self):
+        """Macros: have a name, end with double newline."""
+        macro_name = self._consume_token()
+        # TODO, FIXME: Assert that the next byte after macro_name is a newline
+        #              and consume it.
+        while True:
+            # Start of this loop, last byte was always a newline.
+            next_newline = self.remaining[self.next_token_end:].find('\n')
+            match next_newline:
+                case -1:
+                    # End of file before next newline.
+                    raise self._make_error(
+                        "Macro definition missing null line terminator.")
+                case 0:
+                    # Two newlines in a row indicates end of macro.
+                    self.next_token_end += 1
+                    return (macro_name, self._consume_token())
+                case _ as next_newline:
+                    # Text in the macro
+                    self.next_token_end += next_newline
+
+    def _parse_machine(self, default):
+        # Default doesn't get any machine name, so just use default as the
+        # machine name.
+        if default:
+            machine = "default"
+        else:
+            machine = self._consume_token()
+
+        login = account = password = ''
+
+        while True:
+            # DEBUG: print(f"_parse_machine state {machine, (login, account, password)}")
+            match self._peek_token():
+                case 'login' | 'user':
+                    self._consume()
+                    login = self._consume_token()
+                case 'account':
+                    self._consume()
+                    account = self._consume_token()
+                case 'password':
+                    self._consume()
+                    password = self._consume_token()
+                case '' | 'machine' | 'default' | 'macdef':
+                    return machine, (login, account, password)
+                case _ as unhandled:
+                    raise self._make_error("bad follower token %r" % unhandled)
+
+    def entries(self):
+        while True:
+            match self._consume_token():
+                case "default":
+                    self._consume()
+                    yield "machine", self._parse_machine(True)
+                case "machine":
+                    self._consume()
+                    yield "machine", self._parse_machine(False)
+                case "macdef":
+                    self._consume()
+                    yield "macdef", self._parse_macro()
+                case "":
+                    return
+                case _ as unhandled:
+                    raise self._make_error("bad toplevel token %r" % unhandled)
 
 
 class netrc:
@@ -80,68 +234,23 @@ class netrc:
                 self._parse(file, fp, default_netrc)
 
     def _parse(self, file, fp, default_netrc):
-        lexer = _netrclex(fp)
-        while 1:
-            # Look for a machine, default, or macdef top-level keyword
-            saved_lineno = lexer.lineno
-            toplevel = tt = lexer.get_token()
-            if not tt:
-                break
-            elif tt[0] == '#':
-                if lexer.lineno == saved_lineno and len(tt) == 1:
-                    lexer.instream.readline()
-                continue
-            elif tt == 'machine':
-                entryname = lexer.get_token()
-            elif tt == 'default':
-                entryname = 'default'
-            elif tt == 'macdef':
-                entryname = lexer.get_token()
-                self.macros[entryname] = []
-                while 1:
-                    line = lexer.instream.readline()
-                    if not line:
-                        raise NetrcParseError(
-                            "Macro definition missing null line terminator.",
-                            file, lexer.lineno)
-                    if line == '\n':
-                        # a macro definition finished with consecutive new-line
-                        # characters. The first \n is encountered by the
-                        # readline() method and this is the second \n.
-                        break
-                    self.macros[entryname].append(line)
-                continue
-            else:
-                raise NetrcParseError(
-                    "bad toplevel token %r" % tt, file, lexer.lineno)
+        parser = _netrcparse(file, fp)
+        for kind, entry in parser.entries():
+            match kind:
+                case "machine":
+                    self.hosts[entry[0]] = entry[1]
+                case "macdef":
+                    self.macros[entry[0]] = entry[1]
+                case _ as unhandled:
+                    raise NetrcParseError(
+                        "bad parser return %r" % unhandled, file, self.lineno)
 
-            if not entryname:
-                raise NetrcParseError("missing %r name" % tt, file, lexer.lineno)
+        # FIXME/TODO: Assert hit end of file.
 
-            # We're looking at start of an entry for a named machine or default.
-            login = account = password = ''
-            self.hosts[entryname] = {}
-            while 1:
-                prev_lineno = lexer.lineno
-                tt = lexer.get_token()
-                if tt.startswith('#'):
-                    if lexer.lineno == prev_lineno:
-                        lexer.instream.readline()
-                    continue
-                if tt in {'', 'machine', 'default', 'macdef'}:
-                    self.hosts[entryname] = (login, account, password)
-                    lexer.push_token(tt)
-                    break
-                elif tt == 'login' or tt == 'user':
-                    login = lexer.get_token()
-                elif tt == 'account':
-                    account = lexer.get_token()
-                elif tt == 'password':
-                    password = lexer.get_token()
-                else:
-                    raise NetrcParseError("bad follower token %r" % tt,
-                                          file, lexer.lineno)
-            self._security_check(fp, default_netrc, self.hosts[entryname][0])
+        # FIXME/TODO: Should this be a set of usernames? Shuld it be
+        # called/checked for every machine entry?
+        one_entry = next(iter(self.hosts.keys()))
+        self._security_check(fp, default_netrc, self.hosts[one_entry][0])
 
     def _security_check(self, fp, default_netrc, login):
         if os.name == 'posix' and default_netrc and login != "anonymous":
